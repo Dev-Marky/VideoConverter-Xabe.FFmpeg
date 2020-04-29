fe794d0969a2e46dacac3b7979fa48361f14e9d3
Marky <Dev-Marky>
2020-04-29 20:22:49 +0800

    Added changes for converting movies.

--- ConsoleConverter/ConsoleConverter/Program.cs
+++ ConsoleConverter/ConsoleConverter/Program.cs
@@ -8,6 +8,9 @@ using Xabe.FFmpeg.Enums;
 
 namespace ConsoleConverter {
     internal class Program {
+        protected const string fileSourcePath = "C:\\movies";
+        protected const string fileMovePath = "C:\\DoneConvertedMovies";
+        protected const string fileOutputPath = "C:\\ConvertedMovies";
         private static void Main(string[] args)
         {
             Run().GetAwaiter().GetResult();
@@ -15,13 +18,13 @@ namespace ConsoleConverter {
 
         private static async Task Run()
         {
-            Queue<FileInfo> filesToConvert = new Queue<FileInfo>(GetFilesToConvert("C:\\movies"));
+            Queue<FileInfo> filesToConvert = new Queue<FileInfo>(GetFilesToConvert(fileSourcePath));
             await Console.Out.WriteLineAsync($"Find {filesToConvert.Count} files to convert.");
 
             //Set directory where the app should look for FFmpeg executables.
             FFmpeg.ExecutablesPath = Path.Combine("C:\\movies", "FFmpeg");
             ////Get latest version of FFmpeg. It's great idea if you don't know if you had installed FFmpeg.
-            //await FFmpeg.GetLatestVersion(FFmpegVersion.Full).ConfigureAwait(false);
+            await FFmpeg.GetLatestVersion(FFmpegVersion.Full).ConfigureAwait(false);
 
             //Run conversion
             await RunConversion(filesToConvert).ConfigureAwait(false);
@@ -33,21 +36,21 @@ namespace ConsoleConverter {
         {
             while (filesToConvert.TryDequeue(out FileInfo fileToConvert)) {
                 //Save file to the same location with changed extension
-                string outputFileName = Path.ChangeExtension(fileToConvert.FullName, FileExtensions.Ts);
+                string outputFileName = Path.Combine(fileOutputPath, Path.ChangeExtension(fileToConvert.Name, FileExtensions.Ts));
                 //string result = await Probe.New()
                 //    .Start($"-loglevel error -skip_frame nokey -select_streams v:0 -show_entries frame=pkt_pts_time {fileToConvert}").ConfigureAwait(false);
                 IMediaInfo mediaInfo = await MediaInfo.Get(fileToConvert).ConfigureAwait(false);
                 var videoStream = mediaInfo.VideoStreams.First();
                 var audioStream = mediaInfo.AudioStreams.First();
 
-                //Change some parameters of video stream
-                videoStream
-                    //Rotate video counter clockwise
-                    .Rotate(RotateDegrees.CounterClockwise)
-                    //Set size to 480p
-                    .SetSize(VideoSize.Hd480)
-                    //Set codec which will be used to encode file. If not set it's set automatically according to output file extension
-                    .SetCodec(VideoCodec.H264);
+                ////Change some parameters of video stream
+                //videoStream
+                //    //Rotate video counter clockwise
+                //    .Rotate(RotateDegrees.CounterClockwise)
+                //    //Set size to 480p
+                //    .SetSize(VideoSize.Hd480)
+                //    //Set codec which will be used to encode file. If not set it's set automatically according to output file extension
+                //    .SetCodec(VideoCodec.H264);
 
                 //Create new conversion object
                 var conversion = Conversion.New()
@@ -71,7 +74,17 @@ namespace ConsoleConverter {
                 //Start conversion
                 await conversion.Start().ConfigureAwait(false);
 
-                await Console.Out.WriteLineAsync($"Finished converion file [{fileToConvert.Name}]").ConfigureAwait(false);
+                await Console.Out.WriteLineAsync($"Finished conversion file [{fileToConvert.Name}]").ConfigureAwait(false);
+
+                //await {
+                //    if (!Directory.Exists(fileMovePath)) {
+                //        Directory.CreateDirectory(fileMovePath);
+                //    }
+                //    string movePathMovies = Path.Combine(fileMovePath, fileToConvert.Name);
+                //    File.Move(fileToConvert.FullName, movePathMovies);
+                //    Console.Out.WriteLineAsync("Done moving finished converted movies");
+                //};
+                
             }
         }
 
@@ -80,50 +93,5 @@ namespace ConsoleConverter {
             //Return all files excluding mp4 because I want convert it to mp4
             return new DirectoryInfo(directoryPath).GetFiles();
         }
-
-        //private static async Task RunConversion(Queue filesToConvert)
-        //{
-        //    //foreach (FileInfo data in filesToConvert) {
-        //    //    string outputFileName = Path.ChangeExtension(data.FullName, FileExtensions.Ts);
-        //    //    await Conversion.ToTs(data.FullName, outputFileName).Start();
-        //    //    await Console.Out.WriteLineAsync($"Finished converion file [{data.FullName}]");
-        //    //}
-
-        //    //out FileInfo fileToConvert
-
-        //    foreach(FileInfo file in filesToConvert) {
-        //        //Save file to the same location with changed extension
-        //        string outputFileName = Path.ChangeExtension(file.FullName, FileExtensions.Ts);
-
-        //        //SetOverwriteOutput to overwrite files. It's useful when we already run application before
-        //        var conversion = Conversion.ToTs(file.FullName, outputFileName).SetOverwriteOutput(true);
-        //        //Add log to OnProgress
-        //        conversion.OnProgress += async (sender, args) => {
-        //            //Show all output from FFmpeg to console
-        //            await Console.Out.WriteLineAsync($"[{args.Duration}/{args.TotalLength}][{args.Percent}%] {file.Name}");
-        //        };
-        //        //Start conversion
-        //        await conversion.Start();
-
-        //        await Console.Out.WriteLineAsync($"Finished converion file [{file.Name}]");
-        //    }
-
-        //    //while (out FileInfo fileToConvert in filesToConvert.Dequeue()) {
-        //    //    //Save file to the same location with changed extension
-        //    //    string outputFileName = Path.ChangeExtension(fileToConvert.FullName, ".mp4");
-
-        //    //    //SetOverwriteOutput to overwrite files. It's useful when we already run application before
-        //    //    var conversion = Conversion.ToMp4(fileToConvert.FullName, outputFileName).SetOverwriteOutput(true);
-        //    //    //Add log to OnProgress
-        //    //    conversion.OnProgress += async (sender, args) => {
-        //    //        //Show all output from FFmpeg to console
-        //    //        await Console.Out.WriteLineAsync($"[{args.Duration}/{args.TotalLength}][{args.Percent}%] {fileToConvert.Name}");
-        //    //    };
-        //    //    //Start conversion
-        //    //    await conversion.Start();
-
-        //    //    await Console.Out.WriteLineAsync($"Finished converion file [{fileToConvert.Name}]");
-        //    //}
-        //}
     }
 }
