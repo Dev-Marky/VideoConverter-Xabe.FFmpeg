b6a7739b8dedd792d165ebbc1961410adc8cebe5
Marky <Dev-Marky>
2020-04-30 19:46:09 +0800

    Added changes for moving done converted movie to other folder.

--- ConsoleConverter/ConsoleConverter/Program.cs
+++ ConsoleConverter/ConsoleConverter/Program.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Xabe.FFmpeg;
 using Xabe.FFmpeg.Enums;
 
 namespace ConsoleConverter {
     internal class Program {
-        protected const string fileSourcePath = "C:\\movies";
+        protected const string fileSourcePath = "C:\\Movies";
         protected const string fileMovePath = "C:\\DoneConvertedMovies";
         protected const string fileOutputPath = "C:\\ConvertedMovies";
         private static void Main(string[] args)
@@ -19,15 +20,22 @@ namespace ConsoleConverter {
         private static async Task Run()
         {
             Queue<FileInfo> filesToConvert = new Queue<FileInfo>(GetFilesToConvert(fileSourcePath));
-            await Console.Out.WriteLineAsync($"Find {filesToConvert.Count} files to convert.");
 
-            //Set directory where the app should look for FFmpeg executables.
-            FFmpeg.ExecutablesPath = Path.Combine("C:\\movies", "FFmpeg");
-            ////Get latest version of FFmpeg. It's great idea if you don't know if you had installed FFmpeg.
-            await FFmpeg.GetLatestVersion(FFmpegVersion.Full).ConfigureAwait(false);
+            if (filesToConvert.Count > 0) {
+                await Console.Out.WriteLineAsync($"Find {filesToConvert.Count} files to convert.");
 
-            //Run conversion
-            await RunConversion(filesToConvert).ConfigureAwait(false);
+                //Set directory where the app should look for FFmpeg executables.
+                FFmpeg.ExecutablesPath = Path.Combine(fileSourcePath, "FFmpeg");
+                ////Get latest version of FFmpeg. It's great idea if you don't know if you had installed FFmpeg.
+                await FFmpeg.GetLatestVersion(FFmpegVersion.Full).ConfigureAwait(false);
+
+                //Run conversion
+                await RunConversion(filesToConvert).ConfigureAwait(false);
+            } else {
+                await Console.Out.WriteLineAsync($"No files to convert. Timeout for 10 minutes for now.");
+                Thread.Sleep(60000);
+            }
+            Run().GetAwaiter().GetResult();
 
             Console.In.ReadLine();
         }
@@ -45,10 +53,10 @@ namespace ConsoleConverter {
 
                 ////Change some parameters of video stream
                 //videoStream
-                //    //Rotate video counter clockwise
-                //    .Rotate(RotateDegrees.CounterClockwise)
-                //    //Set size to 480p
-                //    .SetSize(VideoSize.Hd480)
+                //    ////Rotate video counter clockwise
+                //    //.Rotate(RotateDegrees.CounterClockwise)
+                //    ////Set size to 480p
+                //    //.SetSize(VideoSize.Hd720)
                 //    //Set codec which will be used to encode file. If not set it's set automatically according to output file extension
                 //    .SetCodec(VideoCodec.H264);
 
@@ -74,18 +82,31 @@ namespace ConsoleConverter {
                 //Start conversion
                 await conversion.Start().ConfigureAwait(false);
 
+                //Move done converted movies.
+                await TransferDone(fileToConvert.Name, fileToConvert.FullName).ConfigureAwait(false);
                 await Console.Out.WriteLineAsync($"Finished conversion file [{fileToConvert.Name}]").ConfigureAwait(false);
-
-                //await {
-                //    if (!Directory.Exists(fileMovePath)) {
-                //        Directory.CreateDirectory(fileMovePath);
-                //    }
-                //    string movePathMovies = Path.Combine(fileMovePath, fileToConvert.Name);
-                //    File.Move(fileToConvert.FullName, movePathMovies);
-                //    Console.Out.WriteLineAsync("Done moving finished converted movies");
-                //};
                 
             }
+        }
+
+        private static async Task TransferDone(string filename, string fullpath)
+        {
+            if (!Directory.Exists(fileMovePath)) {
+                Directory.CreateDirectory(fileMovePath);
+            }
+            string movePathMovie = Path.Combine(fileMovePath, filename);
+            if (File.Exists(movePathMovie)) {
+                string backupDirectory = fileMovePath + "/backup";
+                if (!Directory.Exists(backupDirectory)) {
+                    Directory.CreateDirectory(backupDirectory);
+                }
+                string backupMovie = Path.Combine(backupDirectory, filename);
+                File.Replace(fullpath, movePathMovie, backupMovie);
+            } else {
+                File.Move(fullpath, movePathMovie);
+            }
+            
+            await Console.Out.WriteLineAsync("Done moving finished converted movies");
         }
 
         private static IEnumerable<FileInfo> GetFilesToConvert(string directoryPath)
